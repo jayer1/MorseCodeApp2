0a70d205a36d2c0251a7030b582fa9de6ccdf9fd
jayer1 <jayer1@my.wctc.edu>
2019-03-09 10:41:42 -0600

    Morse Code App with layout, shared resources and static markup

---
+++ MorseCodeApp/MorseCodeApp/MorseCodeApp.Android/obj/Debug/81/android/src/md54f13f4bac46bde4112ba088c7b4b54b0/MainActivity.java
@@ -0,0 +1,47 @@
+package md54f13f4bac46bde4112ba088c7b4b54b0;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("MorseCodeApp.Droid.MainActivity, MorseCodeApp.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MorseCodeApp.Droid.MainActivity, MorseCodeApp.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
